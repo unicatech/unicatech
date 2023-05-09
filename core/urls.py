9a3ebc3a92cbfc51d85a24aa9a061f5fdde0e486
Unicatech Electronics <unicatechelectronics@gmail.com>
2023-05-09 06:57:19 -0300

    App de Vendas criado. Fazer Venda testado e funcionando.

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import IndexView, ProductListView, AddProductView, EditProductView, ListarContaView, CriarContaView, ComprarDolarView, AdicionarFundosView, EditarContaView, FazerComprasView, ListarComprasView, FazerVendasView, ListarVendasView, ParcelasReceberView, ParcelasReceberModalView
+from .views import IndexView, ProductListView, AddProductView, EditProductView, ListarContaView, CriarContaView, ComprarDolarView, AdicionarFundosView, EditarContaView, FazerComprasView, ListarComprasView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
@@ -13,8 +13,4 @@ urlpatterns = [
     path('adicionarfundos/', AdicionarFundosView.as_view(), name='adicionarfundos'),
     path('fazercompras/', FazerComprasView.as_view(), name='fazercompras'),
     path('listarcompras/', ListarComprasView.as_view(), name='listarcompras'),
-    path('fazervendas/', FazerVendasView.as_view(), name='fazervendas'),
-    path('listarvendas/', ListarVendasView.as_view(), name='listarvendas'),
-    path('parcelasareceber/', ParcelasReceberView.as_view(), name='parcelasareceber'),
-    path('parcelasarecebermodal/', ParcelasReceberModalView.as_view(), name='parcelasarecebermodal'),
 ]
--- unicatech/urls.py
+++ unicatech/urls.py
@@ -22,4 +22,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('', include('Vendas.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

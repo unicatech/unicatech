e3983be568db2d808cca151ec8420f075e804280
Unicatech Electronics <unicatechelectronics@gmail.com>
2023-11-14 19:08:17 -0300

    Container funcionando

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import IndexView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
+

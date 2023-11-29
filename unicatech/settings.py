c53a8ba43ccb54986927a391133fe871ef4ad678
Unicatech Electronics <unicatechelectronics@gmail.com>
2023-11-29 11:13:46 -0300

    Implementado apagar cliente e fornecedor

--- unicatech/settings.py
+++ unicatech/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'unicatech',
         'USER': 'unicatech',
         'PASSWORD': 'abelfera',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }

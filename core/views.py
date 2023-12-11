4c97622ee85d6c491859ebf7004257b332b256cd
Unicatech Electronics <unicatechelectronics@gmail.com>
2023-12-11 13:41:01 -0300

    Nomes de variáveis alterado com sufixo "consolidado" para ser mostrado na página principal

--- core/views.py
+++ core/views.py
@@ -32,7 +32,7 @@ class IndexView(TemplateView):
         #vendas = Venda.objects.filter(criados__month='11').filter(criados__year='2023').filter(ativo=True).order_by('-id')
         vendas_template = []
         venda_lucro = 0
-        venda_lucro_total = 0
+        venda_lucro_consolidado = 0
         venda_total = 0
         #Campo Resumo Lucro Líquido por Venda
         listarVendasTemplate = []
@@ -43,10 +43,10 @@ class IndexView(TemplateView):
         valor_recebido_venda = 0
         quantidade_total_produtos = 0
         total_a_receber = 0
-        valor_excedente_venda_template = 0
-        total_a_receber_template = 0
-        valor_total_venda_template = 0
-        valor_recebido_venda_template = 0
+        valor_excedente_venda_consolidado = 0
+        total_a_receber_consolidado = 0
+        valor_total_venda_consolidado = 0
+        valor_recebido_venda_consolidado = 0
         for venda in vendas:
             quantidade_total_produtos = quantidade_total_produtos + venda.quantidadeProduto
             if identificadorVenda != venda.identificadorVenda:
@@ -57,11 +57,8 @@ class IndexView(TemplateView):
                 for venda in vendaIdentificada:
                     lucro_venda = lucro_venda + venda.lucro
                     valor_total_venda = valor_total_venda + venda.quantidadeProduto * venda.precoProduto
-                    logging.warning("A receber. Produto")
-                    logging.warning(venda.produto_id)
-                    logging.warning(venda.quantidadeProduto * venda.precoProduto)
-                logging.warning("Total da Venda")
-                logging.warning(valor_total_venda)
+                logging.warning("Lucro Venda")
+                logging.warning(lucro_venda)
                 listarVendasTemplate.append(
                     {
                      'venda_id': venda.identificadorVenda,
@@ -73,28 +70,23 @@ class IndexView(TemplateView):
                 #Valores totais a receber
                 recebimentos_venda = MovimentacaoConta.objects.filter(identificadorVenda=venda.identificadorVenda,ativo=True)
                 for recebimento_venda in recebimentos_venda:
-                    logging.warning("Valor Recebido")
-                    logging.warning(recebimento_venda.valorCredito)
                     valor_recebido_venda = valor_recebido_venda + recebimento_venda.valorCredito
-                venda_lucro_total = venda_lucro_total + lucro_venda
+                venda_lucro_consolidado = venda_lucro_consolidado + lucro_venda
 
                 identificadorVenda = venda.identificadorVenda
 
                 total_a_receber = valor_total_venda - valor_recebido_venda
-                logging.warning("Valor Recebido venda")
-                logging.warning(valor_recebido_venda)
-                logging.warning("Total a receber")
-                logging.warning(total_a_receber)
 
                 if total_a_receber < 0:
-                    valor_excedente_venda_template = valor_excedente_venda_template - total_a_receber
+                    valor_excedente_venda_consolidado = valor_excedente_venda_consolidado - total_a_receber
                     total_a_receber = 0
 
-                total_a_receber_template = total_a_receber_template + total_a_receber
-                valor_total_venda_template = valor_total_venda_template + valor_total_venda
-                valor_recebido_venda_template = valor_recebido_venda_template + valor_recebido_venda
+                total_a_receber_consolidado = total_a_receber_consolidado + total_a_receber
+                valor_total_venda_consolidado = valor_total_venda_consolidado + valor_total_venda
+                valor_recebido_venda_consolidado = valor_recebido_venda_consolidado + valor_recebido_venda
                 valor_total_venda = 0
                 valor_recebido_venda = 0
+                lucro_venda = 0
 
         #total_a_receber = valor_total_venda - valor_recebido_venda
         #Cálculo do valor em estoque
@@ -107,11 +99,11 @@ class IndexView(TemplateView):
                                             float(compra.precoProduto) * compra.valorDolarMedio * produto.estoque)
 
         context['vendas'] = listarVendasTemplate
-        context['venda_lucro_total'] = venda_lucro_total
-        context['valor_total_venda'] = valor_total_venda_template
-        context['valor_recebido_venda'] = valor_recebido_venda_template
-        context['total_a_receber'] = total_a_receber_template
-        context['valor_excedente_venda'] = valor_excedente_venda_template
-        context['ticket_medio'] = valor_total_venda / quantidade_total_produtos
+        context['venda_lucro_total'] = venda_lucro_consolidado
+        context['valor_total_venda'] = valor_total_venda_consolidado
+        context['valor_recebido_venda'] = valor_recebido_venda_consolidado
+        context['total_a_receber'] = total_a_receber_consolidado
+        context['valor_excedente_venda'] = valor_excedente_venda_consolidado
+        context['ticket_medio'] = valor_total_venda_consolidado / quantidade_total_produtos
         context['valor_total_estoque'] = valor_total_estoque
         return context

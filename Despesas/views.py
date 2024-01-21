b5fdc331783bbe5457ecc6822a885bda6d868939
Unicatech Electronics <unicatechelectronics@gmail.com>
2024-01-21 20:14:53 -0300

    Se a despesa for apenas uma vez o registro de MovimentacaoConta e Despesa é registrado no ato do cadastro da despesa, não sendo necessária a verificação pela tela inicial.

--- Despesas/views.py
+++ Despesas/views.py
@@ -5,8 +5,9 @@ from django.http import HttpResponseRedirect
 from django.contrib import messages
 
 
-from Despesas.models import CadastroDespesa
-from Contas.models import Conta
+from Despesas.models import CadastroDespesa, Despesa
+from Contas.models import Conta, MovimentacaoConta
+from Contas.views import MovimentacaoFinanceira
 from datetime import date, datetime
 import re
 import logging
@@ -31,6 +32,17 @@ class AdicionarDespesa(TemplateView):
 
     def post(self, request, *args, **kwargs):
         data_modificada = re.sub(r'(\d{1,2})-(\d{1,2})-(\d{4})', '\\3-\\2-\\1', self.request.POST.get("data_despesa"))
+        conta_em_dolar = 0
+        cotacao_dolar = 0
+        tipo_movimentacao = Conta.objects.get(id=self.request.POST.get("conta_despesa"))
+        if tipo_movimentacao.categoria_id > 3:
+            conta_em_dolar = 1
+            valor_dolar = MovimentacaoFinanceira()
+            cotacao_dolar = valor_dolar.dolarMedio()
+        else:
+            conta_em_dolar = 0
+            cotacao_dolar = 1
+
         dataform_despesa = CadastroDespesa(
             criados= data_modificada,
             nome_despesa=self.request.POST.get("nome_despesa"),
@@ -39,7 +51,25 @@ class AdicionarDespesa(TemplateView):
             conta_debito_id=self.request.POST.get("conta_despesa"),
         )
         dataform_despesa.save()
-
+        if self.request.POST.get("periodicidade") == "0":
+            registro_movimentacao = MovimentacaoConta(
+                criados=data_modificada,
+                contaDebito=self.request.POST.get("conta_despesa"),
+                valorDebito=self.request.POST.get("valor_despesa"),
+                identificadorCompra=0,
+                identificadorVenda=0,
+                descricao=self.request.POST.get("nome_despesa"),
+                cotacaoDolar=cotacao_dolar,
+                identificadorDolar=conta_em_dolar,
+            )
+            registro_movimentacao.save()
+            registro_despesa = Despesa(
+                criados=data_modificada,
+                ativo=1,
+                despesa_id=dataform_despesa.id,
+                movimentacao_id=registro_movimentacao.id,
+            )
+            registro_despesa.save()
         #context = super(EditarContaView, self).get_context_data(**kwargs)
         #context["conta"] = Conta.objects.all()
         #context["ategoriaconta"] = CategoriaConta.objects.all()

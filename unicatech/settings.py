b5540d5609ce18682428e79899288f8eae5f63c2
Unicatech Electronics <unicatechelectronics@gmail.com>
2025-08-14 14:23:37 -0300

    upload corrigido para ir pro aws.

--- unicatech/settings.py
+++ unicatech/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'unicatech',
         'USER': 'unicatech',
         'PASSWORD': 'abelfera',
-        'HOST': 'localhost',
+        'HOST': 'unicatech-db',
         'PORT': '5432',
     }
 }

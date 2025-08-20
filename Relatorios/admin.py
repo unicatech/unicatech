a60201e65e6347a8cea758ccdc290b8636a15607
Unicatech Electronics <unicatechelectronics@gmail.com>
2025-08-20 14:46:10 -0300

    Criando um dummy model no Django para app Relatorios de modo a aparecer no admin

--- Relatorios/admin.py
+++ Relatorios/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Dummy
+
+@admin.register(Dummy)
+class DummyAdmin(admin.ModelAdmin):
+    def has_module_permission(self, request):
+        # Faz o app aparecer no menu
+        return True
+
+    def has_view_permission(self, request, obj=None):
+        # Impede erro de acesso à lista (porque não há dados)
+        return False
--- Relatorios/models.py
+++ Relatorios/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Dummy(models.Model):
+    class Meta:
+        managed = False              # não cria tabela no banco
+        verbose_name = "Relatórios"
+        verbose_name_plural = "Relatórios"

f32c12fd941b3d115cfa03b2ec0a68e27eab3ede
Unicatech Electronics <unicatechelectronics@gmail.com>
2025-09-16 22:02:22 -0300

    gunicorn

--- unicatech/settings.py
+++ unicatech/settings.py
@@ -26,7 +26,7 @@ LOGIN_REDIRECT_URL = '/'        # para onde o usuário vai após logar
 SECRET_KEY = 'django-insecure-5bln)qy2@9^6wemx&@(v1to+j4qnmocxm+-8n1jkt7#&*8&vcd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -133,8 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 

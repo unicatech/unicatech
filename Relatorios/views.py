917c8c074b858602c2d6e21aebbe965c79dde491
Unicatech Electronics <unicatechelectronics@gmail.com>
2025-10-04 22:34:15 -0300

    Relatorio de recebimento de cartoers funcionando

--- Relatorios/views.py
+++ Relatorios/views.py
@@ -154,7 +154,7 @@ class RelatorioRecebimentoCartaoView(TemplateView):
         bandeira = self.request.GET.get("bandeira")
 
         # Query base
-        recebimentos = RecebimentoCartao.objects.all().order_by("-criados").filter(ativo=Truese)
+        recebimentos = RecebimentoCartao.objects.all().order_by("-criados").filter(ativo=True)
 
         # Filtro por datas
         if data_inicial:

cd9d6a37f27730d940cd4ab53138d87800b5b94f
eber <unicatechelectronics@gmail.com>
2026-05-22 12:45:06 -0300

    Corrigido bug produto mesmo zerado agora aparece na edição de venda. Apenas os produtos da determinada compra

--- Vendas/views.py
+++ Vendas/views.py
@@ -74,7 +74,7 @@ class FazerVendasView(TemplateView):
                 context['aparelhos'] = 1
             if tipo_produto.categoria_id == 5:
                 context['produtos'] = Produto.objects.all().filter(categoria_id=5).filter(Q(estoque__gt=0) | Q(id__in=ids_produtos)).order_by('NomeProduto')
-            #Se houver recebimento de aparelho associada a Venda
+            # Se houver recebimento de aparelho associada a Venda
             try:
                 compra_venda = MovimentacaoConta.objects.get(identificadorVenda=self.request.GET["idVenda"], identificadorCompra__gt=0,ativo=True)
                 listarProdutosCompraTemplate = []

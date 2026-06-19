d9681a200f4d3ece19e7bce3567d66fa675f78b6
eber <unicatechelectronics@gmail.com>
2026-06-19 14:20:22 -0300

    adicionando core/static

--- unicatech/settings.py
+++ unicatech/settings.py
@@ -136,9 +136,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATICFILES_DIRS = [
-    BASE_DIR / 'core/static',
-]
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
